10eeeb662d8970788582387f3c6edd86182a626e
sheldon <shaldonwilbertdsouza2014@gmail.com>
2024-02-16 13:04:05 +0530

    first commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        id : '1',
+        question : "What has to be broken before you can use it?",
+        answer : "An egg"
+    },
+    {
+        id : '2',
+        question : "What month of the year has 28 days?",
+        answer : "All of them"
+    },
+    {
+        id : '3',
+        question : "What question can you never answer yes to?",
+        answer : "Are you asleep yet"
+    },
+    {
+        id : '4',
+        question : "What is always in front of you but cant be seen?",
+        answer : "The future"
+    },
+    {
+        id : '5',
+        question : "What goes up but never comes down?",
+        answer : "Your age"
+    },
+    {
+        id : '6',
+        question : "What cant talk but will reply when spoken to?",
+        answer : "An echo"
+    }
+]
+
+export default data;
