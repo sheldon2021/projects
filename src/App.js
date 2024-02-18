4c80f1bcb650f14e0ae305a658afaacb45b8063b
sheldon <shaldonwilbertdsouza2014@gmail.com>
2024-02-18 13:32:55 +0530

    Star-Rating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Accordian from './components/accordian'
-import RandomColor from './random-color';
+import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordian /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <StarRating noOfStars={10}/>
     </div>
   );
 }
