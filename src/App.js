b1296d4351b6a509aedd480e0768ab215e4a6afc
sheldon <shaldonwilbertdsouza2014@gmail.com>
2024-02-20 14:36:25 +0530

    Todo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
-import Accordian from './components/accordian'
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+
+// import Accordian from './components/accordian'
+// import RandomColor from './components/random-color';
+// import StarRating from './components/star-rating';
+import Todo from "./components/todo/Todo";
 
 function App() {
   return (
     <div className="App">
-      {/* <Accordian /> */}
-      {/* <RandomColor /> */}
-      <StarRating noOfStars={10}/>
+      <Todo />
     </div>
   );
 }
